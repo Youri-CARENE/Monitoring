16cc1ff97f1e6fe39301078b71a181386f9cad32
Yo17 <youri.carene@gmail.com>
2024-08-27 00:18:55 +0200

    fix  erreurs typographique

--- Mini_Projet_Docker/Build_ALL_Images.sh
+++ Mini_Projet_Docker/Build_ALL_Images.sh
@@ -1,3 +1,3 @@
-docker build -t prometheus -f Dockerfile.prometheus Flask/.
+docker build -t prometheus -f Dockerfile.prometheus Prometheus/.
 docker build -t grafana -f Dockerfile.grafana Grafana/.
-docker build -t flaskapp -f Dockerfile.flask Promotheus/.
+docker build -t flaskapp -f Dockerfile.flask Flask/.
