126e2cff24b9a660e90ad227993d766979bda333
Yo17 <youri.carene@gmail.com>
2024-08-27 00:20:51 +0200

    vérification pour s'assurer que Build_ALL_Images.sh et docker-compose existent avant de les exécuter

--- Mini_Projet_Docker/InstallDemo.sh
+++ Mini_Projet_Docker/InstallDemo.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-# Build les images Docker
-./Build_ALL_Images.sh
+# Vérifie si le fichier Build_ALL_Images.sh existe
+if [ -f "./Build_ALL_Images.sh" ]; then
+  # Build les images Docker
+  ./Build_ALL_Images.sh
+else
+  echo "Build_ALL_Images.sh n'existe pas."
+  exit 1
+fi
 
+# Vérifie si le fichier docker-compose.yml existe
+if [ -f "./docker-compose.yml" ]; then
+  # Lance les conteneurs en arrière-plan
+  docker-compose up -d
+else
+  echo "docker-compose.yml n'existe pas."
+  exit 1
+fi
 
-# Lance les conteneurs en arrière-plan
-docker-compose up -d
-
-
-chmod +x demo.sh
-
-./demo.sh
-
+# Vérifie si le fichier demo.sh existe et exécute-le
+if [ -f "./demo.sh" ]; then
+  chmod +x demo.sh
+  ./demo.sh
+else
+  echo "demo.sh n'existe pas. Passer cette étape."
+fi
 
 # Afficher les logs des conteneurs
 docker-compose logs -f
-
